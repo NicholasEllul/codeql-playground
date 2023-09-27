d0b1ba313f7ec78f82c4e2d81898266a2f8d63b4
Nicholas Ellul <nicholas.ellul1@gmail.com>
2023-09-27 14:36:29 -0400

    introduce JS file

---
+++ script.js
@@ -0,0 +1,3 @@
+// This script takes a URL parameter and directly inserts it into the page without sanitizing it
+var data = new URLSearchParams(window.location.search);
+document.getElementById("demo").innerHTML = data.get("userInput");
